61c8d529f215c3c2016db644f25695e05cf464d6
tanjl <tanjl@itdayang.com>
2023-05-26 19:13:50 +0000

    add singleton

--- test/singleton/main.go
+++ test/singleton/main.go
@@ -1,5 +1,41 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"sync"
+)
 
+type singleton struct {
+	Value int
+}
+
+type Singleton interface {
+	getValue() int
+}
+
+func (s singleton) getValue() int {
+	return s.Value
+}
+
+var (
+	instance *singleton
+	once     sync.Once
+)
+
+func GetInstance(v int) Singleton {
+	once.Do(func() {
+		instance = &singleton{Value: v}
+	})
+
+	return instance
+}
+
+func main() {
+	ins1 := GetInstance(32)
+	ins2 := GetInstance(2)
+	if ins1 != ins2 {
+		log.Fatal("error")
+	}
+	fmt.Println(ins1.getValue(), ins2.getValue(), ins1 == ins2)
 }
